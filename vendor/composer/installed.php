d5a72e80ce6638dfae291eb3c494d175ba04d370
kermage <genealyson.torcende@gmail.com>
2022-04-20 16:26:28 +0800

    track the vendor files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '38d3974c3c7285b2c05a09763375f8d3fee18092',
+        'name' => 'cardanopress-bootstrap/cardanopress-bootstrap',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'cardanopress-bootstrap/cardanopress-bootstrap' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '38d3974c3c7285b2c05a09763375f8d3fee18092',
+            'dev_requirement' => false,
+        ),
+        'chillerlan/php-qrcode' => array(
+            'pretty_version' => '4.3.3',
+            'version' => '4.3.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chillerlan/php-qrcode',
+            'aliases' => array(),
+            'reference' => '6356b246948ac1025882b3f55e7c68ebd4515ae3',
+            'dev_requirement' => false,
+        ),
+        'chillerlan/php-settings-container' => array(
+            'pretty_version' => '2.1.3',
+            'version' => '2.1.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chillerlan/php-settings-container',
+            'aliases' => array(),
+            'reference' => '125dd573b45ffc7cabecf385986a356ba2c6f602',
+            'dev_requirement' => false,
+        ),
+    ),
+);
