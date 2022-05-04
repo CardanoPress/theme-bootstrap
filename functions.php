f52f73cbdd85eb8447d36d724e3c21a93bd33d71
kermage <genealyson.torcende@gmail.com>
2022-05-04 17:19:18 +0800

    disable another enqueue or bootstrap assets

--- functions.php
+++ functions.php
@@ -62,3 +62,6 @@ require_once 'setup/actions-filters.php';
  */
 
 EUM_Handler::run( __FILE__, 'https://raw.githubusercontent.com/pbwebdev/cardanopress-bootstrap-theme/main/update-data.json' );
+
+add_filter( 'cp-governance-enqueue-bootstrap-style', '__return_false' );
+add_filter( 'cp-governance-enqueue-bootstrap-script', '__return_false' );
