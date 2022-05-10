b342aa103f49e65e021f8a125e1fc2f0a030c8fb
kermage <genealyson.torcende@gmail.com>
2022-05-10 16:47:44 +0800

    disable the samples that come from the generator and get the recent versions

--- setup/plugins.php
+++ setup/plugins.php
@@ -24,13 +24,13 @@ if ( ! function_exists( 'cardanopress_bootstrap_plugins' ) ) {
 				'name'             => 'CardanoPress',
 				'slug'             => 'cardanopress',
 				'required'         => true,
-				'source'           => 'https://github.com/pbwebdev/cardanopress/archive/refs/tags/v0.24.0.zip',
+				'source'           => 'https://github.com/pbwebdev/cardanopress/archive/refs/tags/v0.25.0.zip',
 				'force_activation' => true,
 			),
 			array(
 				'name'   => 'CardanoPress - Governance',
 				'slug'   => 'cardanopress-governance',
-				'source' => 'https://github.com/pbwebdev/cardanopress-governance/archive/refs/tags/v0.2.0.zip',
+				'source' => 'https://github.com/pbwebdev/cardanopress-governance/archive/refs/tags/v0.4.0.zip',
 			),
 			array(
 				'name'   => 'Augment Types',
