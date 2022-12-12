050cc0c2e9a9cfdedd11daa743d6bf20d8922599
kermage <genealyson.torcende@gmail.com>
2022-12-12 09:59:29 +0800

    stop offloading jquery

--- setup/scripts-styles.php
+++ setup/scripts-styles.php
@@ -9,15 +9,8 @@
 
 if ( ! function_exists( 'cardanopress_bootstrap_scripts_styles_early' ) ) {
 	function cardanopress_bootstrap_scripts_styles_early() {
-		$suffix = ( SCRIPT_DEBUG || CARDANOPRESS_BOOTSTRAP_THEME_DEBUG ) ? '' : '.min';
 		$theme  = wp_get_theme( CARDANOPRESS_BOOTSTRAP_THEME_BASE );
 
-		// Deregister the jquery version bundled with WordPress
-		wp_deregister_script( 'jquery' );
-		wp_deregister_script( 'jquery-migrate' );
-		// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header
-		wp_register_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery' . $suffix . '.js', array(), '3.6.0', false );
-		wp_register_script( 'jquery-migrate', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.4.0/jquery-migrate' . $suffix . '.js', array(), '3.4.0', false );
 		wp_add_inline_script( 'jquery', 'jQuery.noConflict();' );
 
 		// jQuery
